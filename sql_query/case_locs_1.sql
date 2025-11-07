0b7e103f917ab26f3ee0e3789d8db80e853b054a
Nick C <nick@partnersdentalstudio.com>
2025-11-07 12:29:54 -0600

    updated category field from products table.

--- sql_query/case_locs_1.sql
+++ sql_query/case_locs_1.sql
@@ -1,29 +1,48 @@
-/*  Case Location – One Row Per CaseNumber (Unique)  */
-WITH RankedCases AS
+/* Product Categories */
+WITH ProductCategories AS
+(
+    SELECT 
+        ca.CaseID,
+        ca.CaseNumber,
+        pr.Category
+    FROM dbo.Cases AS ca
+    LEFT JOIN dbo.Caseproducts AS cp
+        ON ca.CaseID = cp.CaseID
+    LEFT JOIN dbo.Products AS pr
+        ON cp.ProductID = pr.ProductID
+    WHERE 
+        ca.Status = 'In Production'
+        AND pr.Category IS NOT NULL
+        AND pr.Category IN ('Metal', 'Clear', 'Wire Bending', 'Marpe', 'Hybrid', 'E2 Expanders', 'Lab to Lab')
+),
+/* Case Location – One Row Per CaseNumber (Unique) */
+RankedCases AS
 (
     SELECT
         ca.CaseNumber as [Case Number],
         ca.PanNumber as [Pan Number],
         ct.Task as [Last Task Completed],
         ct.CompleteDate as [Last Scan Time],
-        ct.Department as [Category],
-        CAST(ca.ShipDate     AS DATE) AS [Ship Date],
+        pc.Category as [Category],
+        CAST(ca.ShipDate AS DATE) AS [Ship Date],
         ca.LastLocationID,
-        cll.Description AS [Last Location],
+        cll.[Description] AS [Last Location],
         ca.[Status],
         ca.LocalDelivery,
         ROW_NUMBER() OVER (
             PARTITION BY ca.CaseNumber
             ORDER BY
-                CASE WHEN ct.CompleteDate IS NULL THEN 1 ELSE 0 END,  -- completed first
-                ct.CompleteDate DESC,                                -- newest date
-                ct.CaseID DESC                                        -- tie-breaker (or use ct.CaseTaskID if exists)
+                CASE WHEN ct.CompleteDate IS NULL THEN 1 ELSE 0 END,
+                ct.CompleteDate DESC,
+                ct.CaseID DESC
         ) AS rn
     FROM dbo.Cases AS ca
     INNER JOIN dbo.CaseTasks AS ct 
         ON ca.CaseID = ct.CaseID
     LEFT JOIN dbo.CaseLogLocations AS cll 
         ON ca.LastLocationID = cll.ID
+    LEFT JOIN ProductCategories AS pc
+        ON ca.CaseID = pc.CaseID
     WHERE ca.Status = 'In Production'
 )
 SELECT
@@ -35,7 +54,7 @@ SELECT
     [Last Location],
     [Last Task Completed],
     [Last Scan Time],
-    LocalDelivery
+    [LocalDelivery]
 FROM RankedCases
 WHERE rn = 1
 ORDER BY [Case Number];
